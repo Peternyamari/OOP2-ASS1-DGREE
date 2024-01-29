244ce1985d3fe7559892c0f3773e0c1a30aae047
Peternyamari <113664817+Peternyamari@users.noreply.github.com>
2024-01-29 13:23:37 +0300

    Update USERINPUT.java

--- USERINPUT.java
+++ USERINPUT.java
@@ -4,8 +4,8 @@ package user.input;
 import java.util.Scanner;
 /*
  * A simple program to demonstrate user input and password verification.
- * If the correct password is entered, the user is welcomed; otherwise,
- * they have a limited number of attempts before the system blocks.
+ * If the correct password and username is entered, the user is welcomed,
+ * user has only 3 number of attempts before the system blocks.
  * @author PETER
  */
 public class USERINPUT {
@@ -14,47 +14,29 @@ public class USERINPUT {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // Creating a Scanner object to read user input from the console
-        Scanner sc=new Scanner(System.in);
+       // Creating a Scanner object to read user input from the console
+        Scanner sc = new Scanner(System.in);
 
-        // First attempt where the correct password is 1234
-        System.out.printf("ENTER PASSWORD:");
-        int PasswordAttempt1=sc.nextInt();
-        
-        // Check if the first password attempt is correct
-        if(PasswordAttempt1==1234){
-            System.out.println("WELCOME USER");
-            // Exit the program if the correct password is entered
-            System.exit(0);
-        } else {
-            // Display a message for an incorrect password attempt
-            System.out.println("incorrect password, attempts remaining 2");
+        // Three attempts for username and password
+        for (int attempt = 1; attempt <= 3; attempt++) {
+            System.out.printf("ENTER USERNAME: ");
+            String username = sc.nextLine();
+            System.out.printf("ENTER PASSWORD: ");
+            int password = sc.nextInt();
+            sc.nextLine(); // Consume the newline character left by nextInt()
+
+            // Check if the username and password are correct
+            if (password == 1234 && username.equals("peter")) {
+                System.out.println("WELCOME USER");
+                // Exit the program if the correct password and username are entered
+                System.exit(0);
+            } else {
+                // Display a message for an incorrect username and password attempt
+                System.out.println("Incorrect username or password, attempts remaining: " + (3 - attempt));
+            }
         }
 
-        // Second password attempt
-        System.out.printf("ENTER PASSWORD:");
-        int PasswordAttempt2=sc.nextInt();
-        
-        // Check if the second password attempt is correct
-        if(PasswordAttempt2==1234){
-            System.out.println("WELCOME USER");
-            System.exit(0);
-        } else {
-            // Display a message for an incorrect password attempt
-            System.out.println("incorrect password, attempts remaining 1");   
-        }
-
-        // Third and final password attempt
-        System.out.printf("ENTER PASSWORD:");
-        int PasswordAttempt3=sc.nextInt();
-        
-        // Check if the third password attempt is correct
-        if(PasswordAttempt3==1234){
-            System.out.println("WELCOME USER");
-            System.exit(0);
-        } else {
-            // Display a message when all attempts are exhausted
-            System.out.println("System blocked! please contact admin!");   
-        }
-    }   
+        // Display a message when all attempts are exhausted
+        System.out.println("System blocked! Please contact admin!");
+    }
 }
